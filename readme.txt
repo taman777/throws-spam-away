f8403c244ee58179afcb78c94d0eb1e1d72fb932
taman777 <info@gti.co.jp>
2019-03-14 01:05:38 +0900

    スパム削除機能

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: tsato
 Donate link: https://www.gti.co.jp/throws-spam-away
 Tags: comments, spam
 Requires at least: 3.1
-Tested up to: 4.9.8
+Tested up to: 5.1.1
 Stable tag: 3.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
--- throws_spam_away_comments.php
+++ throws_spam_away_comments.php
@@ -1,102 +1,94 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: gtimac
- * Date: 2018/08/20
- * Time: 13:43
+ * <p>ThrowsSpamAway</p> Comments Class
+ * WordPress's Plugin
+ * @author Takeshi Satoh@GTI Inc. 2019
+ * @version 3.0
  */
 
 require_once "throws_spam_away.class.php";
 
-    function dce_main()
-    {
-        global $wpdb;
-	    $c_all = $wpdb->query("SELECT comment_ID FROM $wpdb->comments");
-            $c_pend = $wpdb->query("SELECT comment_ID FROM $wpdb->comments WHERE comment_approved = '0'");
-            $s_count = $wpdb->query("SELEcT comment_ID FROM $wpdb->comments WHERE comment_approved =  'spam'");
-?>
+function tsa_comment_main() {
+	global $wpdb;
+	$c_all   = $wpdb->query( "SELECT comment_ID FROM $wpdb->comments" );
+	$c_pend  = $wpdb->query( "SELECT comment_ID FROM $wpdb->comments WHERE comment_approved = '0'" );
+	$s_count = $wpdb->query( "SELEcT comment_ID FROM $wpdb->comments WHERE comment_approved =  'spam'" );
+	?>
 
     <div class="wrap">
-	    <h2>スパムコメント処理</h2>
-	    <p>Akismet などのコメントフィルタによって spam マークがついているコメントを削除します。</p>
-	    <?php if($_POST['c_all'] == 'a' && $_POST['all'] == 's')
-        {
-            if($wpdb->query("DELETE FROM $wpdb->comments WHERE comment_approved =  'spam'") != FALSE)
-	            {
-                    $wpdb->query("DELETE FROM $wpdb->comments WHERE comment_approved =  'spam'");
-                    echo "<p style='color:green'><strong>Spam comments have been deleted.</strong></p>";
-                }
-            else
-                {echo "<p style='color:red'><strong>Something Went Wrong,Please Try Again!</strong></p>";}
-        }
-        elseif($_POST['c_all'] == 'a' && $_POST['all'] == 'a')
-        {
-            if($wpdb->query("DELETE FROM $wpdb->comments") != FALSE)
-	            {
-                    $wpdb->query("DELETE FROM $wpdb->comments");
-                    echo "<p style='color:green'><strong>All comments have been deleted.</strong></p>";
-                }
-            else
-                {echo "<p style='color:red'><strong>Something Went Wrong,Please Try Again!</strong></p>";}
-        }
-        else {
-        ?>
-        <?php
-	        echo "<h4>".__( 'Number of all Comments', ThrowsSpamAway::DOMAIN )." : " . $c_all . " </h4>" ;
-	        echo "<h4>".__( 'Number of Spam Comement', ThrowsSpamAway::DOMAIN )." : ". $s_count . "</h4>";
-	        ?>
+    <h2>スパムコメント処理</h2>
+    <p>Akismet などのコメントフィルタによって spam マークがついているコメントを削除します。</p>
+	<?php if ( $_POST['c_all'] == 'a' && $_POST['all'] == 's' ) {
+		$spam_comment_count = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->comments WHERE comment_approved =  'spam'" );
+		if ( $spam_comment_count == 0 ) {
+			echo "<p style='color:green'><strong>" . __( 'You have no comment spam.', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+        } else {
+		    $delete_flg = $wpdb->query( "DELETE FROM $wpdb->comments WHERE comment_approved =  'spam'" );
+			if ( $delete_flg != false ) {
+				echo "<p style='color:green'><strong>" . __( 'Spam comments have been deleted.', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+			} else {
+				echo "<p style='color:red'><strong>" . __( 'Something Went Wrong,Please Try Again!', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+			}
+		}
+	} elseif ( $_POST['c_all'] == 'a' && $_POST['all'] == 'a' ) {
+		if ( $wpdb->query( "DELETE FROM $wpdb->comments" ) != false ) {
+			$wpdb->query( "DELETE FROM $wpdb->comments" );
+			echo "<p style='color:green'><strong>" . __( 'All comments have been deleted.', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+		} else {
+			echo "<p style='color:red'><strong>" . __( 'Something Went Wrong,Please Try Again!', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+		}
+	}
+		?>
+		<?php
+		echo "<h4>" . __( 'Number of all Comments', ThrowsSpamAway::DOMAIN ) . " : " . $c_all . " </h4>";
+		echo "<h4>" . __( 'Number of Spam Comments', ThrowsSpamAway::DOMAIN ) . " : " . $s_count . "</h4>";
+		?>
 
-        <?php if($c_all > 0) { ?>
+		<?php if ( $c_all > 0 ) { ?>
             <form name="dce" method="post">
                 <input type="hidden" name="c_all" value="a">
-	            <input type="radio" name="all" value="s" /> Delete all spam comments<br>
-	            <input type="radio" name="all" value="a" /> Delete all comments<br>
+                <input type="radio" name="all" value="s"/> <?php _e( 'Delete all spam comments', ThrowsSpamAway::DOMAIN ); ?><br>
+                <input type="radio" name="all" value="a"/> <?php _e( 'Delete all comments', ThrowsSpamAway::DOMAIN ); ?><br>
                 <p class="submit">
-		            <input type="submit" name="Submit" value="<?php _e( 'Delete all Comments', ThrowsSpamAway::DOMAIN ); ?>" />
+                    <input type="submit" name="Submit"
+                           value="<?php _e( 'Delete all Comments', ThrowsSpamAway::DOMAIN ); ?>" onclick="return confirm('<?php _e( 'I will send. Is it OK?', ThrowsSpamAway::DOMAIN ); ?>');"/>
                 </p>
             </form>
-        <?php
-        }
-        else
-        {
-            echo "<p><strong>All comments have been deleted.</strong></p>" ;
-        }
-?>
-<?php if($_POST['c_pend'] == 'p' && $_POST['pend'] == 'p')
-        {
-            if($wpdb->query("DELETE FROM $wpdb->comments WHERE comment_approved = '0'") != FALSE)
-	            {
-                    $wpdb->query("DELETE FROM $wpdb->comments WHERE comment_approved = '0'");
-                    echo "<p style='color:green'><strong>All Pending comments have been deleted.</strong></p>";
-                }
-            else
-                {echo "<p style='color:red'><strong>Something Went Wrong,Please Try Again!</strong></p>";}
-        }
-        else {
-        ?>
-        <?php echo "<h4>".__( 'Number of Pending Comments', ThrowsSpamAway::DOMAIN )." : " . $c_pend . " </h4>" ; ?>
+			<?php
+		} else {
+			echo "<p><strong>". __( 'All comments have been deleted.', ThrowsSpamAway::DOMAIN )."</strong></p>";
+		}
+		?>
+		<?php if ( $_POST['c_pend'] == 'p' && $_POST['pend'] == 'p' ) {
+			if ( $wpdb->query( "DELETE FROM $wpdb->comments WHERE comment_approved = '0'" ) != false ) {
+				$wpdb->query( "DELETE FROM $wpdb->comments WHERE comment_approved = '0'" );
+				echo "<p style='color:green'><strong> ".__( 'All Pending comments have been deleted.', ThrowsSpamAway::DOMAIN )."</strong></p>";
+			} else {
+				echo "<p style='color:red'><strong>".__( 'Something Went Wrong,Please Try Again!', ThrowsSpamAway::DOMAIN )."</strong></p>";
+			}
+		} else {
+			?>
+			<?php echo "<h4>" . __( 'Number of Pending Comments', ThrowsSpamAway::DOMAIN ) . " : " . $c_pend . " </h4>"; ?>
 
-        <?php if($c_pend > 0) { ?>
-            <form name="dcep" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-                <input type="hidden" name="c_pend" value="p">
-                <input type="checkbox" name="pend" value="p" /> Delete all pending comments
-                <p class="submit">
-		            <input type="submit" name="Submit" value="Delete all pending Comments" />
-                </p>
-            </form>
-        <?php
-        }
-        else
-        {
-            echo "<p><strong>All pending comments have been deleted.</strong></p>" ;
-        }
-   }
-?>
-<h4>Warning : Once Comment Deleted can't be restored!</h4>
-    </div>
-<?php
-        }
-    }
+			<?php if ( $c_pend > 0 ) { ?>
+                <form name="dcep" method="post"
+                      action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI'] ); ?>">
+                    <input type="hidden" name="c_pend" value="p">
+                    <input type="checkbox" name="pend" value="p"/> <?php _e( 'Delete all pending comments', ThrowsSpamAway::DOMAIN ); ?>
+                    <p class="submit">
+                        <input type="submit" name="Submit" value="<?php _e( 'Delete all pending Comments', ThrowsSpamAway::DOMAIN ); ?>"/>
+                    </p>
+                </form>
+				<?php
+			} else {
+				echo "<p><strong>" . __( 'All pending comments have been deleted.', ThrowsSpamAway::DOMAIN ) . "</strong></p>";
+			}
+		}
+		?>
+        <h4><?php  _e( 'Warning : Once Comment Deleted can\'t be restored!', ThrowsSpamAway::DOMAIN ); ?></h4>
+        </div>
+		<?php
+}
 
-    // プロセス
-    dce_main();
+// プロセス
+tsa_comment_main();
